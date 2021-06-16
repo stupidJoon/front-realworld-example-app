88f21b2354fa4986cc658f2706aeff7fb96ab523
JunSangYu <junsang.yu3@gmail.com>
2021-06-16 17:50:56 +0900

    :sparkles: ADD bootstrap Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-import { Button } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
-export default function Home() {
-  return <Button variant="primary">Primary</Button>;
+function Home() {
+  return (
+    <Container>
+      <Navbar className="justify-content-between">
+        <Navbar.Brand>conduit</Navbar.Brand>
+        <Nav>
+          <Nav.Item>
+            <Nav.Link className="text-dark">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>Sign in</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>Sign up</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
+    </Container>
+  );
 }
+
+export default Home;
